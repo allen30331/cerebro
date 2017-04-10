bbe31946b6487d576e32e54d3694859147aed8cb
allen30331 <allen30331@gmail.com>
2017-04-10 09:56:32 -0400

    changed file structure

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ var express = require('express');
 var path = require('path');
 var app = express();
 
-app.use(express.static('src/public/views'));
+app.use(express.static('public'));
 
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 8080, function() {
+	console.log('Your app is listening to port 8080')
+});
 
